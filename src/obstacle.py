16e04b6fe33b67bab0d1ac4be2041d308364d338
Xia0ben <10845530+Xia0ben@users.noreply.github.com>
2019-09-13 13:21:12 +0200

    Fix division by inf in edge case of a polygon with two almost exactly same points

--- src/obstacle.py
+++ src/obstacle.py
@@ -62,27 +62,28 @@ class Obstacle(Thing):
             x_b, y_b = polygon.exterior.coords[i + 1]
             x_m, y_m = ((x_a + x_b) / 2.0, (y_a + y_b) / 2.0)
             norm_a_b = np.linalg.norm([x_b - x_a, y_b - y_a])
-            points = [(x_m + d * (y_b - y_a) / norm_a_b, y_m + d * (x_b - x_a) / norm_a_b),
-                      (x_m + d * (y_b - y_a) / norm_a_b, y_m - d * (x_b - x_a) / norm_a_b),
-                      (x_m - d * (y_b - y_a) / norm_a_b, y_m + d * (x_b - x_a) / norm_a_b),
-                      (x_m - d * (y_b - y_a) / norm_a_b, y_m - d * (x_b - x_a) / norm_a_b)]
-            manip_point = (0.0, 1.0)
-            max_dist = 0.0
-            for x_r, y_r in points:
-                scalar_product = (x_b - x_a) * (x_r - x_m) + (y_b - y_a) * (y_r - y_m)
-                if Obstacle._isclose(scalar_product, 0.0):
-                    norm_r_poly_center = np.linalg.norm([poly_center[0] - x_r, poly_center[1] - y_r])
-                    if norm_r_poly_center > max_dist:
-                        manip_point = (x_r, y_r)
-                        max_dist = norm_r_poly_center
+            if norm_a_b != 0.:
+                points = [(x_m + d * (y_b - y_a) / norm_a_b, y_m + d * (x_b - x_a) / norm_a_b),
+                          (x_m + d * (y_b - y_a) / norm_a_b, y_m - d * (x_b - x_a) / norm_a_b),
+                          (x_m - d * (y_b - y_a) / norm_a_b, y_m + d * (x_b - x_a) / norm_a_b),
+                          (x_m - d * (y_b - y_a) / norm_a_b, y_m - d * (x_b - x_a) / norm_a_b)]
+                manip_point = (0.0, 1.0)
+                max_dist = 0.0
+                for x_r, y_r in points:
+                    scalar_product = (x_b - x_a) * (x_r - x_m) + (y_b - y_a) * (y_r - y_m)
+                    if Obstacle._isclose(scalar_product, 0.0):
+                        norm_r_poly_center = np.linalg.norm([poly_center[0] - x_r, poly_center[1] - y_r])
+                        if norm_r_poly_center > max_dist:
+                            manip_point = (x_r, y_r)
+                            max_dist = norm_r_poly_center
 
-            unit_translation = (x_m - manip_point[0], y_m - manip_point[1])
-            unit_translation = (unit_translation / np.linalg.norm(unit_translation)) * manip_unit_length
-            manip_pose = (manip_point[0], manip_point[1], utils.yaw_from_direction(unit_translation))
+                unit_translation = (x_m - manip_point[0], y_m - manip_point[1])
+                unit_translation = (unit_translation / np.linalg.norm(unit_translation)) * manip_unit_length
+                manip_pose = (manip_point[0], manip_point[1], utils.yaw_from_direction(unit_translation))
 
-            actions[tuple(unit_translation)] = manip_pose
-            if not pushes_only:
-                actions[tuple(-1.0 * unit_translation)] = manip_pose
+                actions[tuple(unit_translation)] = manip_pose
+                if not pushes_only:
+                    actions[tuple(-1.0 * unit_translation)] = manip_pose
         return actions
 
     def _compute_q_l(self, world, rp):
