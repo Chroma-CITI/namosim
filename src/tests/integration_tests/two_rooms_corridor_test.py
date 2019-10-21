746cee53a9222684665e8dc3aae8dd81a382445a
Xia0ben <10845530+Xia0ben@users.noreply.github.com>
2019-10-21 18:01:30 +0200

    Add more integration tests for the two rooms corridor scenario

--- src/tests/integration_tests/two_rooms_corridor_test.py
+++ src/tests/integration_tests/two_rooms_corridor_test.py
@@ -4,9 +4,25 @@ from src.simulator import Simulator
 
 class TwoRoomsCorridorTest(unittest.TestCase):
 
-    def test_two_rooms_corridor_test(self):
-        sim = Simulator(simulation_file_path="../data/simulations/first_level/01_two_rooms_corridor/navigation_only_behavior.yaml")
+    def test_navigation_only_behavior(self):
+        sim = Simulator(simulation_file_path="../../../data/simulations/first_level/01_two_rooms_corridor/navigation_only_behavior.yaml")
         sim.run()
+        # Test should end up with a failure
+
+    def test_navigation_only_behavior_no_boxes(self):
+        sim = Simulator(simulation_file_path="../../../data/simulations/first_level/01_two_rooms_corridor/navigation_only_behavior_no_boxes.yaml")
+        sim.run()
+        # Test should end up with a success
+
+    def test_wu_levihn_2014_behavior(self):
+        sim = Simulator(simulation_file_path="../../../data/simulations/first_level/01_two_rooms_corridor/wu_levihn_2014.yaml")
+        sim.run()
+        # Test should end up with a success
+
+    def test_wu_levihn_2014_behavior_no_boxes(self):
+        sim = Simulator(simulation_file_path="../../../data/simulations/first_level/01_two_rooms_corridor/wu_levihn_2014_no_boxes.yaml")
+        sim.run()
+        # Test should end up with a success
 
 
 if __name__ == '__main__':
