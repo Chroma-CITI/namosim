cc6d9887fdb54d62940a6797a4d7b4fe3fc8e784
Xia0ben <10845530+Xia0ben@users.noreply.github.com>
2019-11-04 15:13:49 +0100

    Correctly call manip_search method in tes

--- src/tests/unit_tests/stilman_2005_behavior_test.py
+++ src/tests/unit_tests/stilman_2005_behavior_test.py
@@ -4,6 +4,7 @@ from src.simulator import Simulator
 from src.behaviors.stilman_2005_behavior import Stilman2005Behavior
 from src.utils import utils
 
+
 class Stilman2005BehaviorTest(unittest.TestCase):
 
     def setUp(self):
@@ -16,9 +17,14 @@ class Stilman2005BehaviorTest(unittest.TestCase):
         test_obstacle_uid = ref_world.get_entity_uid_from_name("movable_box")
         connected_components_grid = ref_world.get_connected_components_grid((self.robot_uid,))
         connected_components = ref_world.get_connected_components((self.robot_uid,))
-        goal_cell = utils.real_to_grid(self.behavior.)
+        goal_cell = utils.real_to_grid(self.behavior._navigation_goals[0][0],
+                                       self.behavior._navigation_goals[0][1],
+                                       ref_world.dd.res,
+                                       ref_world.dd.grid_pose)
+        goal_cell_component_id = connected_components_grid[goal_cell[0]][goal_cell[1]]
+        goal_cell_component_cells = connected_components[goal_cell_component_id]
 
-        self.behavior._manip_search(ref_world, test_obstacle_uid, )
+        self.behavior._manip_search(ref_world, test_obstacle_uid, goal_cell_component_cells)
 
 
 if __name__ == '__main__':
