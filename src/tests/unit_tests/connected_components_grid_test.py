3d1f6c52a02770d424a1ea2003b3da1991ed0b9a
Xia0ben <10845530+Xia0ben@users.noreply.github.com>
2019-11-25 17:32:55 +0100

    Fix connected components update test so that we care more about the connected components cells sets than the grids

--- src/tests/unit_tests/connected_components_grid_test.py
+++ src/tests/unit_tests/connected_components_grid_test.py
@@ -18,7 +18,7 @@ class ConnectedComponentsGridTest(unittest.TestCase):
                                                    [0, 0, 0, 0, 0],
                                                    [2, 2, 2, 2, 2],
                                                    [0, 2, 0, 2, 2]]),
-                "expected_result_component": {1: {(0,0), (0,1), (0,2), (0,3), (0,4),
+                "expected_result_components": {1: {(0,0), (0,1), (0,2), (0,3), (0,4),
                                                   (1,0), (1,1), (1,2), (1,3), (1,4)},
                                               2: {(3,0), (3,1), (3,2), (3,3), (3,4),
                                                   (4,1), (4,3), (4,4)}}
@@ -34,7 +34,7 @@ class ConnectedComponentsGridTest(unittest.TestCase):
                                                    [1, 1, 1, 1, 1],
                                                    [1, 1, 1, 1, 1],
                                                    [1, 1, 1, 1, 1]]),
-                "expected_result_component": {
+                "expected_result_components": {
                     1: {(0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
                         (1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
                         (2, 0), (2, 1), (2, 2), (2, 3), (2, 4),
@@ -52,7 +52,7 @@ class ConnectedComponentsGridTest(unittest.TestCase):
                                                    [0, 0, 1, 0, 0],
                                                    [1, 1, 1, 1, 1],
                                                    [0, 1, 0, 1, 1]]),
-                "expected_result_component": {
+                "expected_result_components": {
                     1: {(0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
                         (1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
                                         (2, 2),
@@ -71,7 +71,7 @@ class ConnectedComponentsGridTest(unittest.TestCase):
                                                    [0, 0, 1, 0, 0],
                                                    [2, 2, 0, 3, 3],
                                                    [0, 2, 0, 3, 3]]),
-                "expected_result_component": {
+                "expected_result_components": {
                     1: {(0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
                         (1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
                                         (2, 2)},
@@ -85,14 +85,15 @@ class ConnectedComponentsGridTest(unittest.TestCase):
         self.test_data_for_update = [self.test_data[0], self.test_data[3]]
 
     def test_init_grid(self):
+        print(self.test_init_grid)
         counter = 1
         for test in self.test_data:
             print("Testing case", str(counter))
             ccg = ConnectedComponentsGrid(test["test_array"], neighborhood=utils.TAXI_NEIGHBORHOOD)
             for line_nb in range(ccg.grid.shape[0]):
                 self.assertEqual(list(test["expected_result_array"][line_nb]), list(ccg.grid[line_nb]))
-            for component_id in test["expected_result_component"].keys():
-                self.assertEqual(test["expected_result_component"][component_id], ccg.components[component_id])
+            for component_id in test["expected_result_components"].keys():
+                self.assertEqual(test["expected_result_components"][component_id], ccg.components[component_id])
             counter += 1
 
     def test_update(self):
@@ -102,12 +103,18 @@ class ConnectedComponentsGridTest(unittest.TestCase):
             for test_data_2 in self.test_data:
                 if test_data_1 is not test_data_2:
                     print("Testing case", str(counter_1), "against case", str(counter_2))
+                    # if (counter_1, counter_2) == (2, 1):
+                    #     print()
                     ccg = ConnectedComponentsGrid(test_data_1["test_array"], neighborhood=utils.TAXI_NEIGHBORHOOD)
                     ccg.invaded_cells, ccg.freed_cells = self.compute_invaded_and_freed_cells(
                         test_data_1["expected_result_array"], test_data_2["expected_result_array"])
                     ccg._update_grid()
-                    for line_nb in range(ccg.grid.shape[0]):
-                        self.assertEqual(list(test_data_2["expected_result_array"][line_nb]), list(ccg.grid[line_nb]))
+                    self.assertEqual(len(test_data_2["expected_result_components"]), len(ccg.components))
+                    for expected_result_component in test_data_2["expected_result_components"].values():
+                        self.assertTrue(expected_result_component in ccg.components.values())
+
+                    # for line_nb in range(ccg.grid.shape[0]):
+                    #     self.assertEqual(list(test_data_2["expected_result_array"][line_nb]), list(ccg.grid[line_nb]))
                 counter_2 += 1
             counter_1 += 1
 
