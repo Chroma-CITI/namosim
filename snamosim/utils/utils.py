58e0720b0751d0556b4fc5eaa4de56d67941c49a
Xia0ben <10845530+Xia0ben@users.noreply.github.com>
2020-12-10 19:53:47 +0100

    Add OrderedSet data structure

--- snamosim/utils/utils.py
+++ snamosim/utils/utils.py
@@ -1,16 +1,12 @@
 import math
-from matplotlib.path import Path
-import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw
 import numpy as np
 import os
 import shapely.affinity as affinity
 import mapbox_earcut as earcut
 from shapely.geometry import Polygon
-import time
-import rasterio.features
-import skimage.draw
 import random
+import collections
 
 # Constants
 SQRT_OF_2 = math.sqrt(2.)
@@ -84,6 +80,65 @@ DIRECTIONS = [['NW', 'N', 'NE'],
               ['SW', 'S', 'SE']]
 
 HALF_ONE_UP_TIMES = (0.45, 0.70, 0.90, 1.20)
+
+
+class OrderedSet(collections.MutableSet):
+
+    def __init__(self, iterable=None):
+        self.end = end = []
+        end += [None, end, end]  # sentinel node for doubly linked list
+        self.map = {}  # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:
+            key, prev, next = self.map.pop(key)
+            prev[2] = next
+            next[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = self.end[1][0] if last else self.end[2][0]
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
 
 
 def euclidean_distance(a, b):
@@ -242,11 +297,6 @@ def is_cells_set_colliding_in_grid(cells_set, grid):
         if grid[cell[0]][cell[1]] != 0:
             return True
     return False
-
-
-def matplotlib_show_grid(grid):
-    plt.imshow(grid)
-    plt.show()
 
 
 # region DEPRECATED
