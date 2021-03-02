a24f4c316850a3a65b7fed4dee3492684947d3e1
brenault <benoit.renault@inria.fr>
2021-03-02 15:26:30 +0100

    Start exploring a possible more appropriate rewrite of b2sim using Box2D joints, that were avoided until then because of warnings as to their stability.

--- snamosim/tests/sandbox.py
+++ snamosim/tests/sandbox.py
@@ -1,45 +1,151 @@
-from shapely.geometry import Polygon, MultiPoint, Point, LineString
-import shapely.affinity as affinity
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# C++ version Copyright (c) 2006-2007 Erin Catto http://www.box2d.org
+# Python version by Ken Lauer / sirkne at gmail dot com
+#
+# This software is provided 'as-is', without any express or implied
+# warranty.  In no event will the authors be held liable for any damages
+# arising from the use of this software.
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+# 1. The origin of this software must not be misrepresented; you must not
+# claim that you wrote the original software. If you use this software
+# in a product, an acknowledgment in the product documentation would be
+# appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+# misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
 
-if __name__ == '__main__':
-    p1 = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
+import copy
+import Box2D
+from Box2D.examples.framework import (Framework, Keys, main)
+from Box2D import (b2EdgeShape, b2FixtureDef, b2PolygonShape, b2_dynamicBody,
+                   b2_kinematicBody, b2_staticBody)
 
-    pfin = affinity.rotate(geom=affinity.translate(
-        geom=p1, xoff=1., yoff=2., zoff=0.),
-        angle=215., origin=(0., 0.), use_radians=False
-    )
 
-    pmoitie = affinity.rotate(geom=affinity.translate(
-        geom=p1, xoff=1./2., yoff=2./2., zoff=0.),
-        angle=215./2., origin=(0., 0.), use_radians=False
-    )
+class CollisionPairsContactListener(Box2D.b2ContactListener):
+    def __init__(self, **kwargs):
+        Box2D.b2ContactListener.__init__(self, **kwargs)
+        self._contacts = {}
 
-    pquart = affinity.rotate(geom=affinity.translate(
-        geom=p1, xoff=1./4., yoff=2./4., zoff=0.),
-        angle=215./4., origin=(0., 0.), use_radians=False
-    )
+    def BeginContact(self, contact):
+        self._contacts[(contact.fixtureA.userData['uid'], contact.fixtureB.userData['uid'])] = contact
+        print(self.get_collision_pairs())
 
-    ptroisquarts = affinity.rotate(geom=affinity.translate(
-        geom=p1, xoff=1.*3./4., yoff=2.*3./4., zoff=0.),
-        angle=215.*3./4., origin=(0., 0.), use_radians=False
-    )
+    def EndContact(self, contact):
+        del self._contacts[(contact.fixtureA.userData['uid'], contact.fixtureB.userData['uid'])]
+        print(self.get_collision_pairs())
 
-    polygon_states = [p1, pquart, pmoitie, ptroisquarts, pfin]
+    def get_collision_pairs(self):
+        return list(self._contacts.keys())
 
-    fig, ax = plt.subplots()
 
-    for p in polygon_states:
-        ax.plot(*p.exterior.xy, color='grey')
+class OverlappingEntitiesUidsQueryCallback(Box2D.b2QueryCallback):
+    def __init__(self):
+        Box2D.b2QueryCallback.__init__(self)
+        self.overlapping_uids = set()
 
-    points_states = zip(*[list(polygon.exterior.coords) for polygon in polygon_states])
+    def ReportFixture(self, fixture):
+        self.overlapping_uids.add(fixture.userData['uid'])
+        return True # Continue the query by returning True
 
-    for point_states in points_states:
-        x, y = zip(*[point for point in point_states])
-        ax.scatter(x, y)
 
-    ax.axis('equal')
-    fig.show()
-    print()
+class BodyTypes(Framework):
+    name = "Body Types"
+    description = "Change body type keys: (d) dynamic, (s) static, (k) kinematic"
+    speed = 3  # platform speed
+
+    def __init__(self):
+        self.contact_listener = CollisionPairsContactListener()
+
+        super(BodyTypes, self).__init__()
+
+        self.world.contactListener = self.contact_listener
+        self.world.gravity = (0., 0.)
+
+        # The ground
+        ground = self.world.CreateStaticBody(
+            fixtures=b2FixtureDef(shape=b2EdgeShape(vertices=[(-20, 0), (20, 0)]), isSensor=True, userData={"uid": "ground"})
+        )
+
+        # The attachment
+        # self.attachment = self.world.CreateDynamicBody(
+        #     position=(0, 3),
+        #     fixtures=b2FixtureDef(
+        #         shape=b2PolygonShape(box=(0.5, 2)), density=2.0, isSensor=True, userData={"uid": "attachment"}),
+        # )
+
+        # The platform
+        self.platform = self.world.CreateDynamicBody(
+            position=(0, 5),
+            fixtures=b2FixtureDef(
+                shape=b2PolygonShape(box=(4, 0.5)), isSensor=True, userData={"uid": "platform"}
+            ),
+            bullet=True
+        )
+
+        # The joints joining the attachment/platform and ground/platform
+        # self.world.CreateWeldJoint(
+        #     bodyA=self.attachment,
+        #     bodyB=self.platform,
+        #     anchor=(0, 5)
+        # )
+
+        # self.world.CreatePrismaticJoint(
+        #     bodyA=ground,
+        #     bodyB=self.platform,
+        #     anchor=(0, 5),
+        #     axis=(1, 0),
+        #     maxMotorForce=1000,
+        #     enableMotor=True,
+        #     lowerTranslation=-10,
+        #     upperTranslation=10,
+        #     enableLimit=True
+        # )
+
+        # And the payload that initially sits upon the platform
+        # Reusing the fixture we previously defined above
+        self.payload = self.world.CreateDynamicBody(
+            position=(0, 8),
+            fixtures=b2FixtureDef(
+                shape=b2PolygonShape(box=(0.75, 0.75)), isSensor=True, userData={"uid": "payload"}
+            ),
+            bullet=True
+        )
+
+
+        self.world.CreateDistanceJoint(
+            bodyA=self.payload,
+            bodyB=self.platform,
+            anchorA=(0, 8),
+            anchorB=(0, 5),
+            frequencyHz=0.,
+            dampingRatio=0.
+        )
+
+        self.payload.linearVelocity = self.payload.linearVelocity[0], -1.5
+
+        self.settings.hz = 1
+        self.settings.positionIterations = 1
+        self.settings.velocityIterations = 1
+
+    def Keyboard(self, key):
+        if key == Keys.K_w:
+            self.payload.linearVelocity = 0., 0.
+        elif key == Keys.K_s:
+            self.payload.linearVelocity = self.payload.linearVelocity[0], -5.
+        elif key == Keys.K_d:
+            self.payload.linearVelocity = 5., self.payload.linearVelocity[1]
+        elif key == Keys.K_k:
+            self.platform.type = b2_kinematicBody
+            self.platform.linearVelocity = (-self.speed, 0)
+            self.platform.angularVelocity = 0
+
+    def Step(self, settings):
+        print("self.payload.position: {}, self.platform.position: {}".format(self.payload.position, self.platform.position))
+        super(BodyTypes, self).Step(self.settings)
+
+if __name__ == "__main__":
+    main(BodyTypes)
