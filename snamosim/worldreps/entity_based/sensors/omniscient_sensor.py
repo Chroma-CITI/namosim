040370309e7788f8e4c9cc73c5f404fa30ae0f95
brenault <benoit.renault@inria.fr>
2021-04-16 15:11:21 +0200

    Don't say we updated entities that have not changed.

--- snamosim/worldreps/entity_based/sensors/omniscient_sensor.py
+++ snamosim/worldreps/entity_based/sensors/omniscient_sensor.py
@@ -9,13 +9,15 @@ class OmniscientSensor:
             target_world.add_entity(reference_world.entities[uid].light_copy())
 
         # Update
-        uids_to_update = set(reference_world.entities.keys()).intersection(target_world.entities.keys())
-        for uid in uids_to_update:
+        uids_to_potentially_update = set(reference_world.entities.keys()).intersection(target_world.entities.keys())
+        uids_to_update = set()
+        for uid in uids_to_potentially_update :
             ref_entity = reference_world.entities[uid]
             target_entity = target_world.entities[uid]
             if target_entity.pose != ref_entity.pose:
                 target_entity.pose = ref_entity.pose
                 target_entity.polygon = ref_entity.polygon
+                uids_to_update.add(uid)
 
         # Remove
         uids_to_remove = set(target_world.entities.keys()).difference(reference_world.entities.keys())
