b82fab1a5a38e8598eb392646dd2bbd377e49a70
David Brown <david.brown@inria.fr>
2023-12-06 09:46:18 +0100

    add scenario for moving two obstacles

--- tests/unit/basic_senarios_test.py
+++ tests/unit/basic_senarios_test.py
@@ -55,6 +55,18 @@ class BasicTest(unittest.TestCase):
             == "Agent robot_0 finished executing all its goals."
         )
 
+    def test_1_robot_2_obstacles(self):
+        sim = Simulator(
+            simulation_file_path=os.path.join(
+                self.scenarios_folder, "stilman_only_1_robot_2_obstacles_sim.json"
+            )
+        )
+        sim.run()
+        assert (
+            sim.simulation_log[8].message
+            == "Agent robot_0 finished executing all its goals."
+        )
+
     def test_custom(self):
         sim = Simulator(
             simulation_file_path=os.path.join(self.scenarios_folder, "custom_sim.json")
