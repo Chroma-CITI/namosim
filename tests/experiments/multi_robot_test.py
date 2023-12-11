dbafe8457f1a2c9645b3c700a1757bf2bd7cab03
David Brown <david.brown@inria.fr>
2023-12-11 11:05:45 +0100

    work on migrating after-the-feast

--- tests/experiments/multi_robot_test.py
+++ tests/experiments/multi_robot_test.py
@@ -36,6 +36,24 @@ class MultiRobotTests(unittest.TestCase):
         sim.run()
         assert True
 
+    def test_after_the_feast_2_robots_50_goals(self):
+        config.DISPLAY_WINDOW = True
+        sim = Simulator(
+            simulation_file_path=os.path.join(
+                self.scenarios_folder,
+                "multi_robot/after_the_feast/2_robots_50_goals.svg",
+            )
+        )
+
+        start_time = time.perf_counter()
+        sim.run()
+        end_time = time.perf_counter()
+
+        elapsed_time = end_time - start_time
+
+        print(f"Execution time: {elapsed_time} seconds")
+        assert True
+
 
 if __name__ == "__main__":
     unittest.main()
