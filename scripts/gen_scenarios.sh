36bdf695cbb55c88bb22c8ca78073e2d15cc62cd
David Brown <david.brown@inria.fr>
2024-01-12 09:17:04 +0100

    update scripts for launching experiments

--- scripts/gen_scenarios.sh
+++ scripts/gen_scenarios.sh
@@ -5,58 +5,19 @@ cd $DIR/..
 
 out=tests/experiments/scenarios/intersections/generated
 
-python -m namosim.main gen-alt-scenarios \
-  --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
-  --n-robots 1 \
-  --goals-per-robot 50 \
-  --n-scenarios 10 \
-  --out-dir $out
-python -m namosim.main gen-alt-scenarios \
-  --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
-  --n-robots 1 \
-  --goals-per-robot 50 \
-  --n-scenarios 10 \
-  --use-social-cost \
-  --out-dir $out
-
-python -m namosim.main gen-alt-scenarios \
-  --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
-  --n-robots 4 \
-  --goals-per-robot 50 \
-  --n-scenarios 10 \
-  --out-dir $out
-python -m namosim.main gen-alt-scenarios \
-  --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
-  --n-robots 4 \
-  --goals-per-robot 50 \
-  --n-scenarios 10 \
-  --use-social-cost \
-  --out-dir $out
-
-python -m namosim.main gen-alt-scenarios \
-  --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
-  --n-robots 2 \
-  --goals-per-robot 50 \
-  --n-scenarios 10 \
-  --out-dir $out
-python -m namosim.main gen-alt-scenarios \
-  --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
-  --n-robots 2 \
-  --goals-per-robot 50 \
-  --n-scenarios 10 \
-  --use-social-cost \
-  --out-dir $out
-
-python -m namosim.main gen-alt-scenarios \
-  --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
-  --n-robots 10 \
-  --goals-per-robot 50 \
-  --n-scenarios 10 \
-  --out-dir $out
-python -m namosim.main gen-alt-scenarios \
-  --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
-  --n-robots 10 \
-  --goals-per-robot 50 \
-  --n-scenarios 10 \
-  --use-social-cost \
-  --out-dir $out
+for i in $(seq 1 20); do
+  echo "Generating Intersections scenarios with ${i} robots."
+  python -m namosim.main gen-alt-scenarios \
+    --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
+    --n-robots $i \
+    --goals-per-robot 50 \
+    --n-scenarios 20 \
+    --out-dir $out
+  python -m namosim.main gen-alt-scenarios \
+    --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
+    --n-robots $i \
+    --goals-per-robot 50 \
+    --n-scenarios 20 \
+    --use-social-cost \
+    --out-dir $out
+done
