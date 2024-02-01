a6d8ef6302910ff11bdf5849a94df4b516cfe0b1
David Brown <david.brown@inria.fr>
2024-02-01 08:59:37 +0100

    use a-star search for social evasion

--- scripts/gen_scenarios_citi.sh
+++ scripts/gen_scenarios_citi.sh
@@ -4,17 +4,69 @@ DIR=$(dirname "$0")
 cd $DIR/..
 
 out=tests/experiments/scenarios/citi_lab/generated
+base_scenario="tests/experiments/scenarios/citi_lab/citi_lab_2_robots_50_goals_snamo.svg"
+n_scenarios=10
+n_goals=50
 
-python -m namosim.main gen-alt-scenarios \
-  --base-scenario "tests/experiments/scenarios/citi_lab/citi_lab_2_robots_50_goals_snamo.svg" \
-  --n-robots 2 \
-  --goals-per-robot 50 \
-  --n-scenarios 10 \
-  --out-dir $out
-python -m namosim.main gen-alt-scenarios \
-  --base-scenario "tests/experiments/scenarios/citi_lab/citi_lab_2_robots_50_goals_snamo.svg" \
-  --n-robots 2 \
-  --goals-per-robot 50 \
-  --n-scenarios 10 \
-  --use-social-cost \
-  --out-dir $out
+for i in $(seq 1 10); do
+
+  echo "Generating Citi Lab scenarios with ${i} robots."
+
+  # namo_ncr
+  python -m namosim.main gen-alt-scenarios \
+    --base-scenario ${base_scenario} \
+    --n-robots $i \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
+    --no-resolve-conflicts \
+    --no-resolve-deadlocks \
+    --out-dir $out
+
+  # namo_ndr
+  python -m namosim.main gen-alt-scenarios \
+    --base-scenario ${base_scenario} \
+    --n-robots $i \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
+    --no-resolve-deadlocks \
+    --out-dir $out
+
+  # namo
+  python -m namosim.main gen-alt-scenarios \
+    --base-scenario ${base_scenario} \
+    --n-robots $i \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
+    --out-dir $out
+
+  # snamo_ncr
+  python -m namosim.main gen-alt-scenarios \
+    --base-scenario ${base_scenario} \
+    --n-robots $i \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
+    --use-social-cost \
+    --no-resolve-conflicts \
+    --no-resolve-deadlocks \
+    --out-dir $out
+
+  # snamo_ndr
+  python -m namosim.main gen-alt-scenarios \
+    --base-scenario ${base_scenario} \
+    --n-robots $i \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
+    --use-social-cost \
+    --no-resolve-deadlocks \
+    --out-dir $out
+
+  # snamo
+  python -m namosim.main gen-alt-scenarios \
+    --base-scenario ${base_scenario} \
+    --n-robots $i \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
+    --use-social-cost \
+    --out-dir $out
+
+done
