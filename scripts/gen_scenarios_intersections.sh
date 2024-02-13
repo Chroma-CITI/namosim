c4aabfb2832d372e3fb65f32c9b53b8361c78f8b
David Brown <david.brown@inria.fr>
2024-02-13 09:06:49 +0100

    update scripts

--- scripts/gen_scenarios_intersections.sh
+++ scripts/gen_scenarios_intersections.sh
@@ -3,77 +3,81 @@
 DIR=$(dirname "$0")
 cd $DIR/..
 
-out=tests/experiments/scenarios/intersections/generated
+scenario=intersections
+out=tests/experiments/scenarios/${scenario}/generated
+base_scenario="tests/experiments/scenarios/${scenario}/2_robots_50_goals_snamo.svg"
+n_scenarios=30
+n_goals=50
 
 for i in $(seq 1 10); do
 
-  echo "Generating Intersections scenarios with ${i} robots."
+  echo "Generating ${scenario} scenarios with ${i} robots."
 
   # namo_ncr
   python -m namosim.main gen-alt-scenarios \
-    --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
+    --base-scenario ${base_scenario} \
     --n-robots $i \
-    --goals-per-robot 50 \
-    --n-scenarios 20 \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
     --no-resolve-conflicts \
     --no-resolve-deadlocks \
-    --out-dir "$out/${i}_robots_50_goals_namo_ncr"
+    --out-dir "$out/${i}_robots_${n_goals}_goals_namo_ncr"
 
   # namo_ndr
   python -m namosim.main gen-alt-scenarios \
-    --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
+    --base-scenario ${base_scenario} \
     --n-robots $i \
-    --goals-per-robot 50 \
-    --n-scenarios 20 \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
     --no-resolve-deadlocks \
-    --out-dir "$out/${i}_robots_50_goals_namo_ndr"
+    --out-dir "$out/${i}_robots_${n_goals}_goals_namo_ndr"
 
   # namo
   python -m namosim.main gen-alt-scenarios \
-    --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
+    --base-scenario ${base_scenario} \
     --n-robots $i \
-    --goals-per-robot 50 \
-    --n-scenarios 20 \
-    --out-dir "$out/${i}_robots_50_goals_namo"
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
+    --out-dir "$out/${i}_robots_${n_goals}_goals_namo"
 
   # snamo_ncr
   python -m namosim.main gen-alt-scenarios \
-    --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
+    --base-scenario ${base_scenario} \
     --n-robots $i \
-    --goals-per-robot 50 \
-    --n-scenarios 20 \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
     --use-social-cost \
     --no-resolve-conflicts \
     --no-resolve-deadlocks \
-    --out-dir "$out/${i}_robots_50_goals_snamo_ncr"
+    --out-dir "$out/${i}_robots_${n_goals}_goals_snamo_ncr"
 
   # snamo_ndr
   python -m namosim.main gen-alt-scenarios \
-    --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
+    --base-scenario ${base_scenario} \
     --n-robots $i \
-    --goals-per-robot 50 \
-    --n-scenarios 20 \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
     --use-social-cost \
     --no-resolve-deadlocks \
-    --out-dir "$out/${i}_robots_50_goals_snamo_ndr"
+    --out-dir "$out/${i}_robots_${n_goals}_goals_snamo_ndr"
 
   # snamo
   python -m namosim.main gen-alt-scenarios \
-    --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
+    --base-scenario ${base_scenario} \
     --n-robots $i \
-    --goals-per-robot 50 \
-    --n-scenarios 20 \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
     --use-social-cost \
-    --out-dir "$out/${i}_robots_50_goals_snamo"
+    --out-dir "$out/${i}_robots_${n_goals}_goals_snamo"
 
   # snamo_distance_dr
   python -m namosim.main gen-alt-scenarios \
-    --base-scenario "tests/experiments/scenarios/intersections/2_robots_50_goals_snamo.svg" \
+    --base-scenario ${base_scenario} \
     --n-robots $i \
-    --goals-per-robot 50 \
-    --n-scenarios 20 \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
     --use-social-cost \
     --deadlock-strategy DISTANCE \
-    --out-dir "$out/${i}_robots_50_goals_snamo_distance_dr"
+    --out-dir "$out/${i}_robots_${n_goals}_goals_snamo_distance_dr"
 
 done
--- scripts/gen_scenarios_willow_garage_multi_shape.sh
+++ scripts/gen_scenarios_willow_garage_multi_shape.sh
@@ -10,7 +10,7 @@ n_goals=50
 
 for i in $(seq 1 10); do
 
-  echo "Generating Willow Garage scenarios with ${i} robots."
+  echo "Generating ${scenario} scenarios with ${i} robots."
 
   # namo_ncr
   python -m namosim.main gen-alt-scenarios \
@@ -20,7 +20,7 @@ for i in $(seq 1 10); do
     --n-scenarios ${n_scenarios} \
     --no-resolve-conflicts \
     --no-resolve-deadlocks \
-    --out-dir $out
+    --out-dir "$out/${i}_robots_${n_goals}_goals_namo_ncr"
 
   # namo_ndr
   python -m namosim.main gen-alt-scenarios \
@@ -29,7 +29,7 @@ for i in $(seq 1 10); do
     --goals-per-robot ${n_goals} \
     --n-scenarios ${n_scenarios} \
     --no-resolve-deadlocks \
-    --out-dir $out
+    --out-dir "$out/${i}_robots_${n_goals}_goals_namo_ndr"
 
   # namo
   python -m namosim.main gen-alt-scenarios \
@@ -37,7 +37,7 @@ for i in $(seq 1 10); do
     --n-robots $i \
     --goals-per-robot ${n_goals} \
     --n-scenarios ${n_scenarios} \
-    --out-dir $out
+    --out-dir "$out/${i}_robots_${n_goals}_goals_namo"
 
   # snamo_ncr
   python -m namosim.main gen-alt-scenarios \
@@ -48,7 +48,7 @@ for i in $(seq 1 10); do
     --use-social-cost \
     --no-resolve-conflicts \
     --no-resolve-deadlocks \
-    --out-dir $out
+    --out-dir "$out/${i}_robots_${n_goals}_goals_snamo_ncr"
 
   # snamo_ndr
   python -m namosim.main gen-alt-scenarios \
@@ -58,7 +58,7 @@ for i in $(seq 1 10); do
     --n-scenarios ${n_scenarios} \
     --use-social-cost \
     --no-resolve-deadlocks \
-    --out-dir $out
+    --out-dir "$out/${i}_robots_${n_goals}_goals_snamo_ndr"
 
   # snamo
   python -m namosim.main gen-alt-scenarios \
@@ -67,6 +67,16 @@ for i in $(seq 1 10); do
     --goals-per-robot ${n_goals} \
     --n-scenarios ${n_scenarios} \
     --use-social-cost \
-    --out-dir $out
+    --out-dir "$out/${i}_robots_${n_goals}_goals_snamo"
+
+  # snamo_distance_dr
+  python -m namosim.main gen-alt-scenarios \
+    --base-scenario ${base_scenario} \
+    --n-robots $i \
+    --goals-per-robot ${n_goals} \
+    --n-scenarios ${n_scenarios} \
+    --use-social-cost \
+    --deadlock-strategy DISTANCE \
+    --out-dir "$out/${i}_robots_${n_goals}_goals_snamo_distance_dr"
 
 done
