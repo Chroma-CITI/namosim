4ea304908e870f908b75ecfebdb59de9e2912153
David Brown <davewbrwn@gmail.com>
2025-07-27 22:37:28 +0200

    fix dependencies and colcon test

--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = "namosim"
 
-
 def read_requirements():
     with open("requirements.txt", "r") as file:
-        # Strip whitespace and ignore empty lines or comments
-        return [
-            line.strip() for line in file if line.strip() and not line.startswith("#")
-        ]
-
+        return [line.strip() for line in file if line.strip() and not line.startswith("#")]
 
 setup(
     name=package_name,
     version="0.0.0",
-    packages=find_packages(exclude=["tests"]),
+    packages=find_packages(exclude=["tests", "tests.*"]),  # Exclude tests and subpackages
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        # Include non-Python files like launch files if needed
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
     ],
     install_requires=read_requirements(),
-    maintainer="chroma",
-    maintainer_email="david.brown@inria.fr",
-    description="TODO: Package description",
+    zip_safe=True,
+    maintainer="David Brown",
+    maintainer_email="davewbrwn@gmail.com",
+    description="A mobile-robot motion planner for Navigation Among Obstacles (NAMO)",
     license="MIT",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
 )
