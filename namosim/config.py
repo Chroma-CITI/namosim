e41961b2a551c25c3e519738936e27862768947c
David Brown <david.brown@inria.fr>
2025-08-26 14:26:14 +0200

    docs

--- namosim/config.py
+++ namosim/config.py
@@ -1,4 +1,7 @@
 import os
 
 DISPLAY_WINDOW = os.environ.get("NAMO_NO_DISPLAY_WINDOW", "") == ""
-DEACTIVATE_RVIZ = os.environ.get("NAMO_DEACTIVATE_RVIZ", "") != ""
+DEACTIVATE_RVIZ = (
+    os.environ.get("NAMO_DEACTIVATE_RVIZ", "") != ""
+    or os.environ.get("ROS_DISTRO", "") == ""
+)
